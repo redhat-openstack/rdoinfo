082b8227842fcb6405d9b40b9e782c6bbe98833f
Alfredo Moralejo <amoralej@redhat.com>
2017-03-28 17:55:05 +0200

    Add support for dependencies in separate file and cbs-tags
    This change adds some features to rdoinfo required to automate
    CBS tagging based on gerrit workflows in review.rdoproject.org.
    
    - Adds support to an additional file deps.yaml to store information
    about dependencies in addition to OpenStack packages currently in
    rdo.yml.
    - Adds support for a new attribute cbs-tags both in releases and
    packages that will be used to manage cbs tags applied to CBS builds
    - Adds two new package configs for dependencies.
    
        - unmanaged-dependency: is intended for deps when the spec is
        not managed via gerrit.
        - dependency: for deps managed using gerrit workflows.
    
    Some examples are being included as reference.
    
    Change-Id: I9cda1f85d00a0df0a03f6c38e5bdbb3ccbc2016c

--- verify.py
+++ verify.py
@@ -5,10 +5,36 @@ import yaml
 
 
 def verify(fn):
-    info = rdoinfo.parse_info_file(fn)
+    info = rdoinfo.parse_info_file(fn, deps=True)
     print(yaml.dump(info))
+    cbstags = list_cbs_tags(info)
+    for pkg in info['packages']:
+        verify_cbs_tags(pkg, cbstags)
     print("\n%s looks OK" % fn)
 
+def verify_cbs_tags(pkg, cbstags):
+    if 'cbs-tags' in pkg.keys():
+        reltags = pkg['cbs-tags']
+        for reltag in reltags.keys():
+            for tag in reltags[reltag].keys():
+                try:
+                    cbstags[reltag][tag]
+                except Exception as ex:
+                    raise Exception("cbs-tag %s/%s for package %s does not exist" %
+                                    (reltag, tag, pkg['name']))
+                value = reltags[reltag][tag]
+                if value is None:
+                    raise Exception("cbs-tag %s/%s for package %s is empty" %
+                                    (reltag, tag, pkg['name']))
+    return True
+
+def list_cbs_tags(info):
+    tags = {}
+    for release in info['releases']:
+        if 'cbs-tags' in release.keys():
+            reltags = {release['name']: release['cbs-tags']}
+            tags.update(reltags)
+    return tags
 
 if __name__ == '__main__':
     verify('rdo.yml')
