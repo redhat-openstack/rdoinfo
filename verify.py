b8db4ba870f2d7768ef5c5be3906cd9ebf0b554c
Alfredo Moralejo <amoralej@redhat.com>
2017-03-28 17:55:05 +0200

    Add support for dependencies in separate file and buildsys-tags
    This change adds some features to rdoinfo required to automate
    CBS tagging based on gerrit workflows in review.rdoproject.org.
    
    - Adds support to an additional file deps.yaml to store information
    about dependencies in addition to OpenStack packages currently in
    rdo.yml.
    - Adds support for a new attribute buildsys-tags both in releases and
    packages that will be used to manage cbs tags applied to CBS builds
    - Adds two new package configs for dependencies.
    
        - unmanaged-dependency: is intended for deps when the spec is
        not managed via gerrit.
        - dependency: for deps managed using gerrit workflows.
    
    Some examples are being included as reference.
    
    Change-Id: I9cda1f85d00a0df0a03f6c38e5bdbb3ccbc2016c

--- verify.py
+++ verify.py
@@ -4,11 +4,35 @@ import rdoinfo
 import yaml
 
 
-def verify(fn):
-    info = rdoinfo.parse_info_file(fn)
+def verify(fn, add_fns):
+    info = rdoinfo.parse_info_file(fn, add_fns=add_fns)
     print(yaml.dump(info))
+    buildsystags = list_buildsys_tags(info)
+    for pkg in info['packages']:
+        verify_buildsys_tags(pkg, buildsystags)
     print("\n%s looks OK" % fn)
 
+def verify_buildsys_tags(pkg, buildsystags):
+    if 'buildsys-tags' in pkg.keys():
+        btags = pkg['buildsys-tags']
+        for btag in btags.keys():
+            if btag in buildsystags:
+                value = btags[btag]
+                if value is None:
+                    raise Exception("buildsys-tag %s for package %s is empty" %
+                                    (btag, pkg['name']))
+            else:
+                raise Exception("buildsys-tag %s for package %s does not exist" %
+                                (btag, pkg['name']))
+    return True
+
+def list_buildsys_tags(info):
+    tags = []
+    for release in info['releases']:
+        for repo in release['repos']:
+            if 'buildsys-tags' in repo.keys():
+                tags = tags + repo['buildsys-tags']
+    return tags
 
 if __name__ == '__main__':
-    verify('rdo.yml')
+    verify('rdo.yml', ['deps.yml'])
