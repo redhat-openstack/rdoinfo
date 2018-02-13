05e0231222f57d2e02ba8e935474d9c2cdb111ad
Alfredo Moralejo <amoralej@redhat.com>
2018-02-13 17:00:41 +0100

    Add rocky release to rdoinfo
    Branch stable/queens has been created in upstream requirements
    project so let's add rocky to rdoinfo repo. This patch:
    
    - Adds rocky release
    - Copies queens-uc to rocky-uc
    - Copies queens-uc to queens
    - Copies queens to rocky
    - Removes queens-uc
    
    Change-Id: I705f8da06353c78cb578b2b3574dcc33e2c35c58

--- update-uc.py
+++ update-uc.py
@@ -22,7 +22,7 @@ UC = 'upper-constraints.txt'
 if len(sys.argv) > 1:
     UC_RELEASE = sys.argv[1]
 else:
-    UC_RELEASE = 'queens-uc'
+    UC_RELEASE = 'rocky-uc'
 
 
 # filter for Oslo and clients
