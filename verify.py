9bbd63ab7150eda43e3bc60752f216325416fcc2
Javier Pena <jpena@redhat.com>
2019-09-19 11:14:57 +0200

    Proposal for component definition
    We are defining components inside RDO, as a set of packages that can be
    promoted independently. This commit does so by:
    
    - Defining a components list in rdo.yml.
    - Then, adding a "component: foo" property to those packages belonging
      to the same component.
    - By default, all packages will belong to a "common" component, which
      can then be overriden by each package.
    - We can also have separate components for different tags in the same
      package.
    
    This creates a simple package-component mapping, that can be easily
    parsed later using rdopkg, for example:
    
      $ rdopkg info component:tripleo
    
    Change-Id: I6039341ec10d5e9dc045f0243f21e9e739c29bf7

--- verify.py
+++ verify.py
@@ -12,6 +12,7 @@ def verify(fn, include_fns=[]):
     buildsystags = list_buildsys_tags(inforepo)
     for pkg in inforepo['packages']:
         verify_buildsys_tags(pkg, buildsystags)
+    verify_components(inforepo)
     print("\n%s looks OK" % fn)
 
 def verify_buildsys_tags(pkg, buildsystags):
@@ -27,6 +28,30 @@ def verify_buildsys_tags(pkg, buildsystags):
                 raise Exception("buildsys-tag %s for package %s does not exist" %
                                 (btag, pkg['name']))
     return True
+
+def verify_components(info):
+    # First, create list of components
+    cmp_list = []
+    for component in info['components']:
+        cmp_list.append(component['name'])
+    # Then, make sure all packages belong to a defined component
+    for pkg in info['packages']:
+        if 'component' in pkg:
+            component = pkg['component']
+            if component not in cmp_list:
+                raise Exception("Package %s belongs to a non-existing "
+                                "component %s" % (pkg['name'], component))
+
+        # We can override components on a per-tag basis, check it
+        if 'tags' in pkg:
+            for tag in pkg['tags']:
+                if pkg['tags'][tag] is not None:
+                    if 'component' in pkg['tags'][tag]:
+                        component = pkg['tags'][tag]['component']
+                        if component not in cmp_list:
+                            raise Exception("Package %s in tag %s belongs to "
+                                            "a non-existing component %s" %
+                                            (pkg['name'], tag, component))
 
 def list_buildsys_tags(info):
     tags = ['version-locked']
