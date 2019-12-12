2a473a4e8050169b46eb0203be2c4b319df2e893
Sorin Sbarnea <ssbarnea@redhat.com>
2019-12-12 11:35:37 +0000

    POC: Correct upstream URLs to use opendev.org
    - Use newer location of upstream repositories.
    - Adds test for URL validity that fails even if they are redirected,
      forcing us to update them.
    - Corrects github url ending in .git which are redirected to non .git,
      avoiding one extra redirect.
    - Use pytest with multiprocessing to test the huge number of URLs in
      a decent amount of time.
    
    Change-Id: I789ce96f1b74bd689d54929fc517f2e383b7f9e7

--- verify.py
+++ verify.py
@@ -2,16 +2,21 @@
 from __future__ import print_function
 from distroinfo import info
 import yaml
-
+import requests
+import sys
 
 def verify(fn, include_fns=[]):
     inforepo = info.DistroInfo(
                info_files=[fn] + include_fns,
                local_info='.').get_info()
-    print(yaml.dump(inforepo))
+    # print(yaml.dump(inforepo))
     buildsystags = list_buildsys_tags(inforepo)
     for pkg in inforepo['packages']:
         verify_buildsys_tags(pkg, buildsystags)
+        if 'distgit' in pkg:
+            verify_url(pkg['distgit'])
+        if 'master-distgit' in pkg:
+            verify_url(pkg['master-distgit'])
     print("\n%s looks OK" % fn)
 
 def verify_buildsys_tags(pkg, buildsystags):
@@ -27,6 +32,11 @@ def verify_buildsys_tags(pkg, buildsystags):
                 raise Exception("buildsys-tag %s for package %s does not exist" %
                                 (btag, pkg['name']))
     return True
+
+def verify_url(url):
+    r = requests.head(url)
+    if r.status_code not in [200]:
+        raise Exception("ERROR: %s returned %s\n%s\n%s", url, r, r.headers, r.content)
 
 def list_buildsys_tags(info):
     tags = ['version-locked']
